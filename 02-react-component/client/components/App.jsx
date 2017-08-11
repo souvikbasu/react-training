af8e2d70910ec2cbb5f9338aaff7003fc88e79e1
Souvik Basu <souvikbasu@gmail.com>
2017-08-11 10:18:45 +0530

    Step 02: React Component

---
+++ 02-react-component/client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>This is React Component</h1>
+      </div>);
+  }
+}

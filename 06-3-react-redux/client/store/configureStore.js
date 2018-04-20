9a3cf94fccd5632b2bbb70220a819f3dbd23c449
Souvik Basu <souvikbasu@gmail.com>
2018-04-20 09:59:14 +0530

    remove commented code

--- 06-3-react-redux/client/store/configureStore.js
+++ 06-3-react-redux/client/store/configureStore.js
@@ -4,9 +4,3 @@ import speakers from '../reducers/speakerReducer';
 export default function configureStore(initialState) {
     return createStore(speakers, initialState);
 }
-
-// export default function configureStore(initialState) {
-//     return createStore(speakers, initialState,
-//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//     );
-// }
